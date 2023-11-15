44122d414d3be1def97a743daf480289fdc4a202
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-11-15 13:55:16 +0600

    Modal Form Done
    Modal Form Done

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-import { useState } from "react";
 
 import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 const TableRow = styled.div`
 	display: grid;
@@ -47,7 +48,6 @@ const Discount = styled.div`
 `;
 
 const CabinRow = ({ cabin }) => {
-	const [showFrom, setShowForm] = useState(false);
 	const { isDeleting, deleteCabin } = useDeleteCabin();
 	const { isCreating, createCabin } = useCreateCabin();
 
@@ -88,15 +88,32 @@ const CabinRow = ({ cabin }) => {
 					<button disabled={isCreating} onClick={handelDuplicate}>
 						<HiSquare2Stack />
 					</button>
-					<button onClick={() => setShowForm((show) => !show)}>
-						<HiPencil />
-					</button>
-					<button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-						<HiTrash />
-					</button>
+
+					<Modal>
+						<Modal.Open opens="edit-cabin">
+							<button>
+								<HiPencil />
+							</button>
+						</Modal.Open>
+						<Modal.Window name="edit-cabin">
+							<CreateCabinForm cabinToEdit={cabin} />
+						</Modal.Window>
+
+						<Modal.Open>
+							<button>
+								<HiTrash />
+							</button>
+						</Modal.Open>
+						<Modal.Window>
+							<ConfirmDelete
+								resourceName="cabins"
+								disabled={isDeleting}
+								onConfirm={() => deleteCabin(cabinId)}
+							/>
+						</Modal.Window>
+					</Modal>
 				</div>
 			</TableRow>
-			{showFrom && <CreateCabinForm cabinToEdit={cabin} />}
 		</>
 	);
 };
