545657c9e9e306bd1cec7019788a9782a4bcc7ce
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-11-24 18:38:40 +0600

    Computing Recent Bookings and Stays
    Computing Recent Bookings and Stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,15 +1,14 @@
-import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
 
-const StyledDashboardLayout = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-template-rows: auto 34rem auto;
-	gap: 2.4rem;
-`;
-
-import React from "react";
+import Spinner from "../../ui/Spinner";
 
 const DashboardLayout = () => {
+	const { isLoading: isLoading1, bookings } = useRecentBookings();
+	const { isLoading: isLoading2, stays, confirmedStays } = useRecentStays();
+
+	if (isLoading1 || isLoading2) return <Spinner />;
+
 	return (
 		<div className="grid grid-cols-4 grid-rows-[auto_34rem_auto] gap-10">
 			<div className="">Statistics</div>
